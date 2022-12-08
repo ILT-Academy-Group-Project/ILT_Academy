d4733f799ce76b3f50262323cf7c0b798940334c
Sam Phipps <samuel.v.phipps@gmail.com>
2022-12-08 13:31:15 -0600

    router naming

---
+++ server/routes/announcements.router.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET route template
+ */
+router.get('/', (req, res) => {
+  // GET route code here
+});
+
+/**
+ * POST route template
+ */
+router.post('/', (req, res) => {
+  // POST route code here
+});
+
+/**
+ * DELETE route 
+ */
+
+
+
+/**
+ * PUT route 
+ */
+
+
+module.exports = router;
