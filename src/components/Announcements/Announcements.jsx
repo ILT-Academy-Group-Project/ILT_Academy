0140238da237a6e0daccd882d4da3fd7a9974370
Juan Villegas <jvillegas422@gmail.com>
2022-12-12 10:39:19 -0600

    Setup base Announcements folder/page

---
+++ src/components/Announcements/Announcements.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector} from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+function Announcements() {
+    console.log('in announcements');
+
+    return (
+        <>
+            <h1>Announcements Page</h1>
+        </>
+    );
+}
+
+export default Announcements;
