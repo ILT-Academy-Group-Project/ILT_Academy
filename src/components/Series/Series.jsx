70ef3e1e0eb558e5a274433bdf28ea58c1a839fd
Jordan Wolter <jordan.wolter@icloud.com>
2022-12-13 10:20:10 -0600

    style admin home

--- src/components/Series/Series.jsx
+++ src/components/Series/Series.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Button } from '@mui/material';
 import { useHistory } from 'react-router-dom';
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { ThemeProvider } from '@mui/system';
+import { PrimaryMainTheme } from '../PrimaryMainTheme/PrimaryMainTheme';
 
 
 function Series() {
@@ -18,14 +28,23 @@ function Series() {
     }, []);
 
     return (
-        
-        <> 
+
+        <>
+        <ThemeProvider theme={PrimaryMainTheme}>
+        <h1>Series</h1>
             {series.map(serial => (
-                <Button 
-                variant='outlined'
-                onClick={() => history.push(`/admin/modules/${serial.id}`)}>{serial.seriesName}
-                </ Button>
+                <Grid2 direction='column'>
+                    <Button
+                    sx={{minHeight: 100, fontSize: 60}}
+                    color='primary'
+                    fullWidth='true'
+                        variant='outlined'
+                        onClick={() => history.push(`/admin/modules/${serial.id}`)}>{serial.seriesName}
+                    </ Button>
+                </Grid2>
+
             ))}
+            </ThemeProvider>
         </>
     )
 };
